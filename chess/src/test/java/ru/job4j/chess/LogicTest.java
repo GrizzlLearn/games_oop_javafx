db00ea4d5d22590e4288cf95705d91427b58f4e0
GrizzlLearn <dylebedev.learning@gmail.com>
2022-06-06 22:32:24 +0300

    2. Каркас шахматной доски [#792] free done

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,6 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
 public class LogicTest {
 
@@ -14,5 +15,29 @@ public class LogicTest {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void testImpossibleMoveException() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        bishopBlack.way(Cell.F4);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void testOccupiedCellException()
+            throws OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        logic.add(new PawnBlack(Cell.B7));
+        logic.move(Cell.C8, Cell.A6);
+    }
+
+
+    @Test(expected = FigureNotFoundException.class)
+    public void testFigureNotFoundException()
+            throws OccupiedCellException, FigureNotFoundException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C8));
+        logic.move(Cell.C8, Cell.A6);
     }
 }
