506e738bd9d229f7473dd3cc276b88446a4bf666
GrizzlLearn <dylebedev.learning@gmail.com>
2022-06-06 21:13:32 +0300

    2. Каркас шахматной доски [#792] way done

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -18,9 +18,25 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell dest) {
-        throw new ImpossibleMoveException(
-                String.format("Could not way by diagonal from %s to %s", position, dest)
-        );
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not way by diagonal from %s to %s", position, dest)
+            );
+        }
+        int size = Math.abs(dest.getX() - this.position().getX());
+        Cell[] steps = new Cell[size];
+
+        int deltaX = this.position.getX() < dest.getX() ? 1 : -1;
+
+        int deltaY = this.position.getY() < dest.getY() ? 1 : -1;
+
+        for (int index = 0; index < size; index++) {
+            steps[index] = Cell.findBy(this.position.getX() + deltaX, this.position.getY() + deltaY);
+            deltaX += deltaX > 0 ? 1 : -1;
+            deltaY += deltaY > 0 ? 1 : -1;
+        }
+        return steps;
+
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
