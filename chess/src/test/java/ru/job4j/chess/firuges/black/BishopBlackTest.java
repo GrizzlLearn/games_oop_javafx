49992fe62851b27ebbca9aed90adda6097bc39b8
GrizzlLearn <dylebedev.learning@gmail.com>
2022-06-06 15:07:45 +0300

    2. Каркас шахматной доски [#792] add tests for way

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -20,4 +20,12 @@ public class BishopBlackTest {
         BishopBlack expected = (BishopBlack) bishopBlack.copy(Cell.G2);
         assertThat(expected.position(), is(Cell.G2));
     }
+
+    @Test
+    public void testWay() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.B1);
+        Cell[] expected = new Cell[] {Cell.C2, Cell.D3, Cell.E4};
+        Cell[] actual = bishopBlack.way(Cell.E4);
+        assertThat(expected, is(actual));
+    }
 }
